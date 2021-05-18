3910c130d8593be638a4c9f8d0dbf66988801aa2
Jackie Sun <jackiesun2@gmail.com>
2021-05-18 14:31:42 +1000

    added listing views and controllers, added feature to create new listing and show listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,32 @@
 class ListingsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit]
+
   def index
+    @listings = Listing.all
   end
 
   def new
+    @listing = Listing.new
   end
 
   def edit
   end
 
-  def show
+  def create
+    @listing = current_user.listing.create(listing_params)
+    if @listing.valid?
+      redirect_to @listing
+    else
+      render:new
+    end
   end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :location)
+  end
+
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :booking, dependent: :destroy
   validates :title, :description, :location, presence: true
+
 end
